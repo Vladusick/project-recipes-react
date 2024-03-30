b1de5014c4159fcb46721a7b8ec40aa9b01260b9
Vladusick <samuelfs@mail.ru>
2024-03-30 23:19:30 +0400

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { NotFound } from "./pages/NotFound";
+
 function App() {
-  return <div className="App">initial</div>;
+  return (
+    <>
+      <Header />
+      <main className="container content">
+        <Router>
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route path="/about" Component={About} />
+            <Route path="/contacts" Component={Contact} />
+            <Route path="*" Component={NotFound} />
+          </Routes>
+        </Router>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
