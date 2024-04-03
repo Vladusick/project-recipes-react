d55812bffb03d2afd1d0b3e983b9a631114a58bc
Vladusick <samuelfs@mail.ru>
2024-04-03 16:33:43 +0400

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { NotFound } from "./pages/NotFound";
+import { Movie } from "./pages/Movie";
 
 function App() {
   return (
@@ -14,9 +15,15 @@ function App() {
         <Header />
         <main className="container content">
           <Routes>
-            <Route exact path="/" Component={Home} />
+            {/* <Route exact path="/" Component={Home} /> */}
+            <Route path="/" element={<Home></Home>}>
+              <Route path="test" element={<About />}>
+                {/* <Route path='new' element={<NewPost />} /> */}
+              </Route>
+            </Route>
             <Route path="/about" Component={About} />
             <Route path="/contacts" Component={Contact} />
+            <Route path="/movies/:title" Component={Movie} />
             <Route path="*" Component={NotFound} />
           </Routes>
         </main>
