1d7d8f6321edac4e4b25feb59b5ec799cc9362dc
Vladusick <samuelfs@mail.ru>
2024-04-19 17:12:04 +0400

    removed unused pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,24 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
 import { Home } from "./pages/Home";
-import { About } from "./pages/About";
-import { Contact } from "./pages/Contact";
 import { NotFound } from "./pages/NotFound";
 import { Category } from "./pages/Category";
 import { Recipe } from "./pages/Recipe";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <main className="container content">
-          <Routes>
-            {/* <Route exact path="/" Component={Home} /> */}
-            <Route path="/" element={<Home></Home>}>
-              <Route path="test" element={<About />}>
-                {/* <Route path='new' element={<NewPost />} /> */}
-              </Route>
-            </Route>
-            <Route path="/about" Component={About} />
-            <Route path="/contacts" Component={Contact} />
-            <Route path="/category/:name" Component={Category} />
-            <Route path="/meal/:id" Component={Recipe} />
-            <Route path="*" Component={NotFound} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
-    </>
+    <Router basename="/project-recipes-react">
+      <Header />
+      <main className="container content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/category/:name" Component={Category} />
+          <Route path="/meal/:id" Component={Recipe} />
+          <Route path="*" Component={NotFound} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
